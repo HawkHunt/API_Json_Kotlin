50293e5747573730ce14a3225954d8f53f3c3008
HawkHunt <cobraluuk@hotmail.com>
2020-08-28 14:40:56 +0200

    Replaced GraphView library MPAndroidChart library

--- app/src/main/java/com/example/api_json_kotlin/GraphShow.kt
+++ app/src/main/java/com/example/api_json_kotlin/GraphShow.kt
@@ -1,21 +1,26 @@
 package com.example.api_json_kotlin
 
+import android.R.attr
+import android.R.attr.data
 import android.content.Intent
 import android.graphics.Color
-import android.graphics.Paint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.jjoe64.graphview.DefaultLabelFormatter
-import com.jjoe64.graphview.GraphView
-import com.jjoe64.graphview.GridLabelRenderer
-import com.jjoe64.graphview.series.BarGraphSeries
-import com.jjoe64.graphview.series.DataPoint
-import com.jjoe64.graphview.series.LineGraphSeries
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import kotlinx.android.synthetic.main.activity_graph_show.*
+
 
 class GraphShow : AppCompatActivity() {
 
     var playerId = ""
+
+
+    fun doATest(A: Int, B: Int){
+        var C = A * B
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,31 +40,40 @@ class GraphShow : AppCompatActivity() {
         startActivity(i_GraphShow)
     }
 
-    fun doATest(A:Int, B:Int ){
-        var C = A * B
-    }
-
     fun drawGraph() {
         var finalData = intent.extras?.getParcelableArrayList<PlayerShipJson>("ExtraData")
         playerId = intent.getStringExtra("playerID")
 
-        var graph = findViewById<GraphView>(R.id.graph)
-
-        var series = LineGraphSeries<DataPoint>()
 
 
-        var testSeries = BarGraphSeries<DataPoint>()
+        val entries = mutableListOf<Entry>()
 
         if (finalData != null) {
             for (i in 0 until finalData.size){
+                var newEntry = Entry(finalData[i].playerShipJsonClassData.shipTier.toFloat(),finalData[i].playerShipJsonClassData.winRate.toFloat() )
+                entries.add(i, newEntry )
+            }
+        }
 
+        var dataSet = LineDataSet(entries, "Winrate of ships from: ${finalData?.get(0)?.playerShipJsonClassData?.nation}")
+        dataSet.color = Color.WHITE
+        var lineData = LineData(dataSet)
+        chart.data = lineData
+        chart.invalidate()
+
+        //var graph = findViewById<GraphView>(R.id.graph)
+
+        //var series = LineGraphSeries<DataPoint>()
+
+        /*if (finalData != null) {
+            for (i in 0 until finalData.size){
                 var x = finalData[i].playerShipJsonClassData.shipTier.toDouble()
                 var y = finalData[i].playerShipJsonClassData.winRate.toDouble()
                 series.appendData(DataPoint(x, y), true, 10)
             }
         }
 
-        // add percentage character to vertical axis
+        add percentage character to vertical axis
         graph.gridLabelRenderer.labelFormatter = object : DefaultLabelFormatter() {
             override fun formatLabel(value: Double, isValueX: Boolean): String {
                 return if (isValueX) {
@@ -79,7 +93,7 @@ class GraphShow : AppCompatActivity() {
         series.dataPointsRadius = 100F
         series.dataPointsRadius = 10F
         series.thickness = 5
-        graph.addSeries(series)
+        graph.addSeries(series)*/
 
     }
 }
--- app/src/main/java/com/example/api_json_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/api_json_kotlin/MainActivity.kt
@@ -65,7 +65,6 @@ open class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
